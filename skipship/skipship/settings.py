9cd7b9bd8e0156f8e382b32fd9ac7997104f38dd
Youssef_Sawiris <youssef.sawiris@students.makeschool.com>
2020-02-16 21:10:50 -0800

    updated database

--- skipship/skipship/settings.py
+++ skipship/skipship/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'SkipShip.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'media.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'cart.sqlite3'),
     }
 }
 
