2d8e091b0de5e6864e517303339fc29d7b1d1eb7
Youssef_Sawiris <youssef.sawiris@students.makeschool.com>
2020-03-03 20:49:29 -0800

    add urls

--- skipship/cart/urls.py
+++ skipship/cart/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 # Cart Urls
 urlpatterns = [
-    path('', views.Homepage, name="homepage"),
-    path('cart/', views.ListCart, name='list-carts'),
+    path('', views.Homepage.as_view(), name="homepage"),
+    path('cart/', views.ListCart.as_view(), name='list-carts'),
     path('cart/<int:pk>/', views.DetailCart.as_view(), name='detail-cart'),
     path('cart/create/', views.CreateCart.as_view(), name='create-cart'),
     path('cart/<int:pk>/update/', views.Updatecart.as_view(), name='update-cart'),
--- skipship/cartitem/urls.py
+++ skipship/cartitem/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns=[
-    path('cartitem/', views.ListCartItem.as_view(), name='list-cartitem'),
-    path('cartitem/<int:pk>/', views.DetailCartItem.as_view(), name='detail-cartitem'),
-    path('cartitem/create/', views.CreateCartItem.as_view(), name='create-cartitem'),
-    path('cartitem/<int:pk>/update/', views.UpdateCartItem.as_view(), name='update-cartitem'),
-    path('cartitem/<int:pk>/delete/', views.DeleteCartItem.as_view(), name='delete-cartitem'),
+    path('all/', views.ListCartItem.as_view(), name='list-cartitem'),
+    path('<int:pk>/', views.DetailCartItem.as_view(), name='detail-cartitem'),
+    path('create/', views.CreateCartItem.as_view(), name='create-cartitem'),
+    path('<int:pk>/update/', views.UpdateCartItem.as_view(), name='update-cartitem'),
+    path('<int:pk>/delete/', views.DeleteCartItem.as_view(), name='delete-cartitem'),
 ]
